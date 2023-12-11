561a885a2bfc13a6fd5de6bec50ed3234553cf95
Majid Ash <Magid.Ash.Si@gmail.com>
2023-12-11 11:28:40 -0800

    Added display of contacts

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -3,9 +3,16 @@ function ContactsList({contacts}) {
   return (
     <div>
        <h3>Contacts List</h3>
-       <ul>
-          {contacts.map(contact => <li key={contact.id}>{contact.name}</li>)}
-       </ul>
+       {
+        contacts.length ? (<ul>
+          {contacts.map(contact => <li key={contact.id}>
+            <p>{contact.name} {contact.lastName}</p>
+            <p><span>📭</span> {contact.email}</p>
+            <p><span>📞</span> {contact.phone}</p>
+            <button>🗑</button>
+          </li>)}
+       </ul>) : <p>No Contacts Yet!</p>
+       }
     </div>
   )
 }
